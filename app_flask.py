24866c48cbd5266ea6eee3e237d400f2c98b9d56
Miguelals <miguel.alves.silva78p@gmail.com>
2025-11-18 14:08:18 +0000

    Elisa vitoria ferreira modulo 2

--- app_flask.py
+++ app_flask.py
@@ -2,12 +2,42 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/soma")
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+
+@app.route("/soma")
+def somar():
+    numero1 = request.args.get("cebolinha",type=int)
+    numero2 = request.args.get("nomeQualquer",type=int)
+    total= numero1 + numero2
+    return {"operecao":"soma","resultado":total}
+
+@app.route("/subtrair")
+def subtrair():
+    numero1 = request.args.get("valor1", type=int)
+    numero2 = request.args.get("valor2",type=int)
+    total = numero1 - numero2
+    return {"operacao":"subtracao","resultado":f"{numero1}-{numero2} = {total}"}
+
+@app.route("/mutiplicar")
+def mutiplicar():
+    numero1 = request.args.get("valor1", type=int)
+    numero2 = request.args.get("valor2",type=int)
+    total = numero1 * numero2
+    return {"operacao":"mutplicacao","resultado":f"{numero1}X{numero2} = {total}"}
+
+@app.route("/dividir")
+def dividir():
+    numero1 = request.args.get("valor1",type=int)
+    numero2 = request.args.get("valor2",type=int)
+    if numero2 == 0:
+       total = numero1 / numero2
+    
+    return {"operacao":"divisao","resultado":f"{numero1}%{numero2} = {total}"}
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+  
